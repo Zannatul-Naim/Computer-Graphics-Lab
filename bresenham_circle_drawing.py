6f4298a72b059e54a947cff865795a047f5c86d4
Zannatul-Naim <zannatulnaim09@gmail.com>
2025-10-13 00:42:50 +0600

    bresenham's circle-drawing updated

--- bresenham_circle_drawing.py
+++ bresenham_circle_drawing.py
@@ -1,49 +1,88 @@
-import turtle
-import time
+import turtle 
+import time 
 
-# Setup
 screen = turtle.Screen()
-screen.title("Bresenham's Circle (Simplified)")
-screen.setup(800, 600)
-screen.setworldcoordinates(0, 0, 800, 600)
+screen.setup(1000, 800)
+screen.setworldcoordinates(0, 0, 1000, 800)
 
 t = turtle.Turtle()
 t.speed(0)
 t.hideturtle()
-t.penup()
 
 def draw_circle(xc, yc, r):
     x = 0
-    y = r
-    p = 1 - r  # Initial decision parameter
+    y = r 
+    p = 1-r 
 
-    # Loop for the first octant
-    while x <= y:
-        # Plot the 8 symmetric points for the current (x, y)
-        # This is done by adding/subtracting x and y from the center (xc, yc)
-        t.goto(xc + x, yc + y); t.dot(3)
-        t.goto(xc - x, yc + y); t.dot(3)
-        t.goto(xc + x, yc - y); t.dot(3)
-        t.goto(xc - x, yc - y); t.dot(3)
-        t.goto(xc + y, yc + x); t.dot(3)
-        t.goto(xc - y, yc + x); t.dot(3)
-        t.goto(xc + y, yc - x); t.dot(3)
-        t.goto(xc - y, yc - x); t.dot(3)
-        time.sleep(0.1)
+    t.penup()
+    while x<=y:
+        t.goto(xc+x, yc+y); t.dot(3)
+        t.goto(xc-x, yc-y); t.dot(3)
+        t.goto(xc-x, yc+y); t.dot(3)
+        t.goto(xc+x, yc-y); t.dot(3)
+        t.goto(xc+y, yc+x); t.dot(3)
+        t.goto(xc-y, yc-x); t.dot(3)
+        t.goto(xc+y, yc-x); t.dot(3)
+        t.goto(xc-y, yc+x); t.dot(3)
+        time.sleep(0.005)
 
-        # Update the decision parameter and coordinates
         if p < 0:
-            p = p + 2 * x + 3
+            p = p+2*x+3
         else:
-            p = p + 2 * x - 2 * y + 5
+            p = p+2*x-2*y+5
             y -= 1
         x += 1
 
-if __name__ == "__main__":
-    # Draw a few circles to show it works
-    draw_circle(200, 450, 75)
+draw_circle(500, 400, 75)
+screen.exitonclick()
+
+
+# import turtle
+# import time
+
+# # Setup
+# screen = turtle.Screen()
+# screen.title("Bresenham's Circle")
+# screen.setup(800, 600)
+# screen.setworldcoordinates(0, 0, 800, 600)
+
+# t = turtle.Turtle()
+# t.speed(0)
+# t.hideturtle()
+# t.penup()
+
+# def draw_circle(xc, yc, r):
+#     x = 0
+#     y = r
+#     p = 1 - r  # Initial decision parameter
+
+#     # Loop for the first octant
+#     while x <= y:
+#         # Plot the 8 symmetric points for the current (x, y)
+#         # This is done by adding/subtracting x and y from the center (xc, yc)
+#         t.goto(xc + x, yc + y); t.dot(3)
+#         t.goto(xc - x, yc + y); t.dot(3)
+#         t.goto(xc + x, yc - y); t.dot(3)
+#         t.goto(xc - x, yc - y); t.dot(3)
+#         t.goto(xc + y, yc + x); t.dot(3)
+#         t.goto(xc - y, yc + x); t.dot(3)
+#         t.goto(xc + y, yc - x); t.dot(3)
+#         t.goto(xc - y, yc - x); t.dot(3)
+#         time.sleep(0.1)
+
+#         # Update the decision parameter and coordinates
+#         if p < 0:
+#             p = p + 2 * x + 3
+#         else:
+#             p = p + 2 * x - 2 * y + 5
+#             y -= 1
+#         x += 1
+
+# if __name__ == "__main__":
+#     # Draw a few circles to show it works
+#     draw_circle(200, 450, 75)
     
-    screen.exitonclick()
+#     screen.exitonclick()
 
 
 
