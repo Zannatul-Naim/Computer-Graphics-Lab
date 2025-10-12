c63dafda8f35d3b592366b47f0e29969669e72cf
Zannatul-Naim <zannatulnaim09@gmail.com>
2025-10-12 21:16:27 +0600

    modified sutherland hodgman polyghon clipping

--- sutherland_hodgman_polygon_clipping.py
+++ sutherland_hodgman_polygon_clipping.py
@@ -1,40 +1,42 @@
-import turtle
+import turtle 
+import time 
 
-# Setup
 screen = turtle.Screen()
-screen.setup(600, 500)
-screen.setworldcoordinates(0, 0, 500, 400)
+screen.setup(1000, 800)
+screen.setworldcoordinates(0, 0, 1000, 800)
+
 t = turtle.Turtle()
 t.hideturtle()
 t.speed(0)
 
-# Clipping window
-clip_x1, clip_y1 = 100, 100
-clip_x2, clip_y2 = 300, 300
+# clipping window
+wx1, wy1 = 350, 250
+wx2, wy2 = 650, 450
 
-# Original polygon
-polygon = [(150, 150), (250, 80), (350, 200), (280, 320), (120, 280)]
+# original polygon
+polygon = [(300, 350), (500, 200), (700, 350), (500, 500)]
 
 def inside(x, y, edge):
-    if edge == 0: return x >= clip_x1
-    if edge == 1: return y >= clip_y1
-    if edge == 2: return x <= clip_x2
-    return y <= clip_y2
+    if edge == 0: return x >= wx1 
+    if edge == 1: return y >= wy1 
+    if edge == 2: return x <= wx2 
+    return y <= wy2 
 
 def intersect(x1, y1, x2, y2, edge):
-    dx, dy = x2 - x1, y2 - y1
-    if edge == 0: t = (clip_x1 - x1) / dx
-    elif edge == 1: t = (clip_y1 - y1) / dy
-    elif edge == 2: t = (clip_x2 - x1) / dx
-    else: t = (clip_y2 - y1) / dy
-    return (x1 + t * dx, y1 + t * dy)
+    dx, dy = x2-x1, y2-y1 
 
-def clip_edge(pts, edge):
+    if edge == 0: t = (wx1-x1)/dx 
+    elif edge == 1: t = (wy1-y1)/dy 
+    elif edge == 2: t = (wx2-x1)/dx 
+    else: t = (wy2-y1)/dy 
+    return (x1+t*dx, y1+t*dy)
+
+def clip_edge(points, edge):
     out = []
-    x1, y1 = pts[-1]
-    for x2, y2 in pts:
+    x1, y1 = points[-1]
+    for x2, y2 in points:
         in1, in2 = inside(x1, y1, edge), inside(x2, y2, edge)
-        if in2:
+        if in2: 
             if not in1: out.append(intersect(x1, y1, x2, y2, edge))
             out.append((x2, y2))
         elif in1:
@@ -42,21 +44,14 @@ def clip_edge(pts, edge):
         x1, y1 = x2, y2
     return out
 
-# Sutherland-Hodgman algorithm
-clipped = polygon
-for e in range(4):
-    clipped = clip_edge(clipped, e)
-
-# Draw clipping window
-t.color("red")
+# draw clippint window
 t.penup()
-t.goto(clip_x1, clip_y1)
+t.goto(wx1, wy1)
 t.pendown()
-for pos in [(clip_x2, clip_y1), (clip_x2, clip_y2), (clip_x1, clip_y2), (clip_x1, clip_y1)]:
-    t.goto(pos)
+t.goto(wx2, wy1), t.goto(wx2, wy2), t.goto(wx1, wy2), t.goto(wx1, wy1)
 
-# Draw original polygon
-t.color("blue")
+# draw original polygon
+t.color('blue')
 t.penup()
 t.goto(polygon[0])
 t.pendown()
@@ -64,14 +59,20 @@ for p in polygon:
     t.goto(p)
 t.goto(polygon[0])
 
-# Draw clipped polygon
+# sutherland-hodgman algorithm
+clipped = polygon 
+for edge in range(4):
+    clipped = clip_edge(clipped, edge)
+
+# draw clipped polygon
 t.color("green")
-t.width(3)
+t.pensize(3)
 t.penup()
 t.goto(clipped[0])
 t.pendown()
 for p in clipped:
     t.goto(p)
+    time.sleep(0.5)
 t.goto(clipped[0])
 
-turtle.done()
+screen.exitonclick()
